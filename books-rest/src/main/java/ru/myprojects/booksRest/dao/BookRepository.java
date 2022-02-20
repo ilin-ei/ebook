7a515a4c1b1706e8b55b7c86b516ca4fb67dcb61
ilin-ei <90279281+ilin-ei@users.noreply.github.com>
2022-02-20 22:38:02 +0300

    Delete books-rest directory

--- books-rest/src/main/java/ru/myprojects/booksRest/dao/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.myprojects.booksRest.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ru.myprojects.booksRest.entity.Book;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-
-}
